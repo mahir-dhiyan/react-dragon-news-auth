64c93f9fbcc3f66687562117c35f3aecb3660256
mahir-dhiyan <mahird.chowdhury@gmail.com>
2024-06-18 15:25:18 +0600

    updated firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,14 +3,17 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+console.log('Inside Firebase ', import.meta.env.VITE_PASS);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDK3pjktq-e8CeB1hglYvJ1CaopT6sX3UQ",
-  authDomain: "react-dragon-news-auth-c0f5e.firebaseapp.com",
-  projectId: "react-dragon-news-auth-c0f5e",
-  storageBucket: "react-dragon-news-auth-c0f5e.appspot.com",
-  messagingSenderId: "210226068350",
-  appId: "1:210226068350:web:fa67492a92f582ebe1d12f"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
